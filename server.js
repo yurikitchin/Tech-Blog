624be56174397f68c0ed86a8c720563c9f539bf1
Yuri Kitchin <ykitchin@gmail.com>
2021-07-20 20:03:15 +0800

    add sequelize and handlebars

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-//const hbs = exphbs.create({ helpers }); 
+ 
 
 //set up paths
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 //Set up the express app
 const app = express();
@@ -29,15 +30,15 @@ const sess = {
 app.use(session(sess));
 
 //sets handlebars as template engine
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening at Port', PORT ));
 });
