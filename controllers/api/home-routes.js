974b1275481256c3c7d59fb8caf11841ed8f23f4
Yuri Kitchin <ykitchin@gmail.com>
2021-07-20 20:04:28 +0800

    homeroutes

--- controllers/api/home-routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').router()
-//require models when they have been created
-//const { modelName1, Modelname2, modelname 3 } = require("../models")
-
-
-// ================= Homepage =======================
