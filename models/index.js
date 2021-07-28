7747c62061471b6a421800ef26c35eac19e8fcb3
Yuri Kitchin <ykitchin@gmail.com>
2021-07-28 20:32:45 +0800

    fix spelling mistakes in the models

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ User.hasMany(Post, {foreignKey: "user_id"})
 
 User.hasMany(Comment, {foreignKey: "user_id"})
 
-Post.hasMany(Comment, {foreignKey: "podt_id"})
+Post.hasMany(Comment, {foreignKey: "post_id"})
 
 Post.belongsTo(User, {foreignKey: "user_id"})
 
