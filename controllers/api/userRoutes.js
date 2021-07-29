1c73a86dc6a5f6a90cf471d8dd899906b62ba745
Yuri Kitchin <ykitchin@gmail.com>
2021-07-29 16:00:57 +0800

    delete code to check for existing username,

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,27 +5,15 @@ require("dotenv").config();
 //create new user
 router.post("/users", async (req, res) => {
   try {
-    const exisitingUsers = await User.findOne({
-      where: { userName: req.body.userName },
-    });
-
-    const userData = await User.create({
+     const userData = await User.create({
       userName: req.body.userName,
       password: req.body.password,
     });
-    console.log("this is the existing users", exisitingUsers);
-    console.log("this is user data", userData);
-    if (exisitingUsers.userName === userData.userName) {
-      let err = "User name already exists, please try new user name";
-      console.log(err);
-      res.status(500).json(err);
-    } else {
-      req.session.save(() => {
-        req.session.loggedIn = true;
+    req.session.save(() => {
+      req.session.loggedIn = true;
 
-        res.status(200).json(userData);
-      });
-    }
+      res.status(200).json(userData);
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -35,31 +23,38 @@ router.post("/users", async (req, res) => {
 //login
 router.post("/users/login", async (req, res) => {
   try {
-    const UserData = await User.findOne({ where: {userName: req.body.userName} })
+    const UserData = await User.findOne({
+      where: { userName: req.body.userName },
+    });
 
     if (!UserData) {
-      res.status(400).json({ message: "incorrect email or password. please try again"})
-      return
+      res
+        .status(400)
+        .json({ message: "incorrect email or password. please try again" });
+      return;
     }
-    const validPassword = await UserData.checkPassword(req.body.password)
+    const validPassword = await UserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: "incorrect email or password. please try again"})
-      return
+      res
+        .status(400)
+        .json({ message: "incorrect email or password. please try again" });
+      return;
     }
 
     req.session.save(() => {
       req.session.loggedIn = true;
       console.log("user logged in");
 
-      res.status(200).json({ user: UserData, message: "You are now logged in!" });
+      res
+        .status(200)
+        .json({ user: UserData, message: "You are now logged in!" });
     });
-
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
-})
+});
 module.exports = router;
 
 //Log Out
@@ -72,3 +67,5 @@ router.post("/logout", (req, res) => {
     res.status(404).end();
   }
 });
+
+//Add a comment
